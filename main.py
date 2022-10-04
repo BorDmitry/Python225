204b472546b43acf2944685f07d75313ff80c55d
DMITRY <borisovd58@gmail.comgit config --global user.email borisovd58@gmail.com>
2022-10-05 01:04:52 +0400

    DZ_Rectangle.py

--- main.py
+++ main.py
@@ -204,6 +204,7 @@
 
 
 # [10:47] Козякина Елена
+#
 # class Point:
 #     def __init__(self, x=0, y=0):
 #         self.x = x
@@ -255,28 +256,28 @@
 
             # Д.Задание из класса
            # -------------------------
-class Weight:
-    def __init__(self, k=0):
-        self.__k = k
-
-    @ property
-    def kg_k(self):
-        return self.__k
-
-    @kg_k.setter
-    def kg_k(self, k):
-        if isinstance (k, int, float):
-            self.__k = k
-        else:
-            print("Килограммы должны быть заданны числами")
-
-    def to_pounds(self):
-        return  self.__k * 2.205
-
-
-w1 = Weight(12)
-print(w1.kg_k, "кг => ", end='')
-print(w1.to_pounds(), "фунтов")
+# class Weight:
+#     def __init__(self, k=0):
+#         self.__k = k
+#
+#     @ property
+#     def kg_k(self):
+#         return self.__k
+#
+#     @kg_k.setter
+#     def kg_k(self, k):
+#         if isinstance (k, int, float):
+#             self.__k = k
+#         else:
+#             print("Килограммы должны быть заданны числами")
+#
+#     def to_pounds(self):
+#         return  self.__k * 2.205
+#
+#
+# w1 = Weight(12)
+# print(w1.kg_k, "кг => ", end='')
+# print(w1.to_pounds(), "фунтов")
 
 
            # 2.10.22 методы в классах
