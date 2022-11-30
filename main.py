e57e27b3be3a73b9b4883eca2c6489a4b05d9929
DMITRY <borisovd58@gmail.comgit config --global user.email borisovd58@gmail.com>
2022-11-30 13:30:38 +0400

    D_Z_Parsing.py

--- main.py
+++ main.py
@@ -1858,3 +1858,49 @@
 
 # Модули и наследование
 
+#____________________________________________________
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+import re
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def refined(s):
+    res = re.sub(r"\D+", "", s)
+    return res
+
+
+def write_csv(data):
+    with open('plugins.csv', 'a') as f:
+        writer = csv.writer(f, lineterminator="\r", delimiter=";")
+        writer.writerow((data['name'], data['url'], data['rating']))
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    p1 = soup.find_all('section', class_="plugin-section")[1]
+    plugins = p1.find_all('article')
+
+    for plugin in plugins:
+        name = plugin.find("h3").text
+        url = plugin.find("h3").find("a").get('href')
+        rating = plugin.find("span", class_="rating-count").find("a").text
+        r = refined(rating)
+
+        data = {'name': name, 'url': url, 'rating': r}
+        write_csv(data)
+
+
+def main():
+    url = "https://ru.wordpress.org/plugins/"
+    get_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
