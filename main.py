e1fcf7fb16edab2edb11628a0cc4cdea89bffc87
DMITRY <borisovd58@gmail.comgit config --global user.email borisovd58@gmail.com>
2022-12-03 00:07:37 +0400

    D_Z_4_ParsingPages.py + run.py

--- main.py
+++ main.py
@@ -1860,46 +1860,60 @@
 
 #____________________________________________________
 
-from bs4 import BeautifulSoup
-import requests
-import csv
-import re
+# from bs4 import BeautifulSoup
+# import requests
+# import csv
+# import re
+#
+#
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def refined(s):
+#     res = re.sub(r"\D+", "", s)
+#     return res
+#
+#
+# def write_csv(data):
+#     with open('plugins.csv', 'a') as f:
+#         writer = csv.writer(f, lineterminator="\r", delimiter=";")
+#         writer.writerow((data['name'], data['url'], data['rating']))
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, 'lxml')
+#     p1 = soup.find_all('section', class_="plugin-section")[1]
+#     plugins = p1.find_all('article')
+#
+#     for plugin in plugins:
+#         name = plugin.find("h3").text
+#         url = plugin.find("h3").find("a").get('href')
+#         rating = plugin.find("span", class_="rating-count").find("a").text
+#         r = refined(rating)
+#
+#         data = {'name': name, 'url': url, 'rating': r}
+#         write_csv(data)
+#
+#
+# def main():
+#     url = "https://ru.wordpress.org/plugins/"
+#     get_data(get_html(url))
+#
+#
+# if __name__ == '__main__':
+#     main()
 
 
-def get_html(url):
-    r = requests.get(url)
-    return r.text
-
-
-def refined(s):
-    res = re.sub(r"\D+", "", s)
-    return res
-
-
-def write_csv(data):
-    with open('plugins.csv', 'a') as f:
-        writer = csv.writer(f, lineterminator="\r", delimiter=";")
-        writer.writerow((data['name'], data['url'], data['rating']))
-
-
-def get_data(html):
-    soup = BeautifulSoup(html, 'lxml')
-    p1 = soup.find_all('section', class_="plugin-section")[1]
-    plugins = p1.find_all('article')
-
-    for plugin in plugins:
-        name = plugin.find("h3").text
-        url = plugin.find("h3").find("a").get('href')
-        rating = plugin.find("span", class_="rating-count").find("a").text
-        r = refined(rating)
-
-        data = {'name': name, 'url': url, 'rating': r}
-        write_csv(data)
+#---------------------------------------------------------
+from D_Z_4_ParsingPages import Parser
 
 
 def main():
-    url = "https://ru.wordpress.org/plugins/"
-    get_data(get_html(url))
+    for i in range(1, 3):
+        pars = Parser(f"https://www.ixbt.com/live/index/news/page{i}", "news_of_day.txt")
+        pars.run()
 
 
 if __name__ == '__main__':
