e96ee938b172b06ec8889ab7c4e99d129deeadaf
DMITRY <borisovd58@gmail.comgit config --global user.email borisovd58@gmail.com>
2022-12-14 16:24:15 +0400

    d_z_db4.txt

--- main.py
+++ main.py
@@ -1908,13 +1908,13 @@
 
 #---------------------------------------------------------
 from D_Z_4_ParsingPages import Parser
-
-
-def main():
-    for i in range(1, 3):
-        pars = Parser(f"https://www.ixbt.com/live/index/news/page{i}", "news_of_day.txt")
-        pars.run()
-
-
-if __name__ == '__main__':
-    main()
+#
+#
+# def main():
+#     for i in range(1, 3):
+#         pars = Parser(f"https://www.ixbt.com/live/index/news/page{i}", "news_of_day.txt")
+#         pars.run()
+#
+#
+# if __name__ == '__main__':
+#     main()
